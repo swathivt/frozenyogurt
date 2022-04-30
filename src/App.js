98393c0ace07c5742ee7bd3d05e7ca1a46202a18
Swathy V <svanc212@mail.com>
2022-04-30 02:21:24 -0700

    Page content banner fixed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import './css/style.css';
 import './css/mobile.css';
 import { NavBar } from './components/navbar/NavBar.js';
-import {BigHome} from './components/keli/BigHome.js';
+import { BigHome } from './components/keli/BigHome.js';
 
 function App() {
   return (
     <div className="App">
       <div id="page">
         <NavBar />
-        <div id="body">
-        <BigHome />
-          {/* Keli */}
+        <div id="body" className='home'>
+          <BigHome />
+          {/* Content Body - Component */}
+          {/* Content Footer - Component */}
         </div>
         <footer className="">
           {/* Jeel */}
--- src/components/keli/BigHome.js
+++ src/components/keli/BigHome.js
@@ -1,39 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import bgHome from '../../images/bg-home.jpg';
-
-/*
-const homepagepicture = {
-    src: "../../images/bg-home.jpg",
-    alt: 'Home Page Picture'
-  };
-
-/*
-function chgPic(e){
-    e.target.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Endangered_Red_Panda.jpg');
-    //e.target.setAttribute('src', 'frozenyogurt/src/images/bg-home.jpg');
-    e.target.setAttribute('alt', 'Second Home Page Picture');
-}
-*/
+import React from 'react';
 
 export class BigHome extends React.Component {
-    render() {
-      return (
-        <div id="body" class="home">
-          <h1>
-          <a href="">Frozen Yogurt</a>
-          </h1>
-          <div className="header">
-          <img //onClick ={chgPic}
-            src={bgHome} 
-            alt="" />
+  render() {
+    return (
+        <div className="header">
+          <img src={bgHome} alt="" />
+          <div>
+            <a href="product.html">Freeze Delight</a>
           </div>
         </div>
-      );
-    }
-  };
-  
-  ReactDOM.render(
-    <BigHome />,
-    document.getElementById('root')
-  );
+    );
+  }
+}
